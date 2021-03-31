9f4f818ecea5e8f43ab8c8e5c383915211c79e94
Irfan Harith <irfanharith97@gmail.com>
2021-03-31 17:55:15 +0800

    sql connection optimization

--- Application/src/mysql/index.js
+++ Application/src/mysql/index.js
@@ -1,72 +1,41 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const mysql = require("mysql");
-const cors = require("cors");
- 
+const mysql = require('mysql');
+const cors = require('cors');
+
 app.use(cors());
 app.use(express.json()); //convert from json
- 
+
 const db = mysql.createConnection({
-  user: "root",
-  host: "localhost",
-  password: "admin",
-  database: "testdb",
+  user: 'root',
+  host: 'localhost',
+  password: 'admin',
+  database: 'testdb',
 });
 
+var table = 'users';
 
-app.post("/insert", (req, res) => {
-  const insertQuery = "INSERT INTO users SET ?";
-  db.query(insertQuery, req.body, (err, result) => {
+const postSQL = {
+  type: 'INSERT INTO ' + table + ' SET ?',
+  msj: 'Added to Database',
+};
+
+
+function sqlQuery(db, postSQL,req, res) {
+  db.query(postSQL.type, req.body, (err, result) => {
     if (err) {
-      console.log(err);
+      res.send(err);
     } else {
-      res.send("Added to Database");
+      res.send(postSQL.msj);
     }
   });
-});
+}
 
- 
-app.get("/select", (req, res) => {
-  db.query("SELECT * FROM users", (err, result) => {
-    if (err) {
-      console.log(err);
-    } else {
-      res.send(result);
-    }
-  });
-});
-
-app.put("/update", (req, res) => {
-  const updateQuery =
-    "UPDATE users SET email = ?, pass = ? WHERE id = ?";
-  db.query(
-    updateQuery,
-    [req.body.email, req.body.pass, req.body.id],
-    (err, result) => {
-      if (err) {
-        console.log(err);
-      } else {
-        res.send(result);
-      }
-    }
-  );
-});
-
-app.delete("/delete/:id", (req, res) => {
-  db.query(
-    "DELETE FROM users WHERE id = ?",
-    req.params.id,
-    (err, result) => {
-      if (err) {
-        console.log(err);
-      } else {
-        res.send(result);
-      }
-    }
-  );
+app.post('/reviews', (req, res) => {
+  sqlQuery(db, postSQL, req, res);
 });
 
 
 app.listen(3003, () => {
-  console.log("Yey, your server is running on port 3003");
+  console.log('Yey, your server is running on port 3003');
 });
